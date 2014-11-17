d54d77c0b902e29cff539c47e9782bb3bfac8b58
Dmitry Antonenko <banguit@gmail.com>
2014-11-18 01:14:42 +0200

    - Application loading chages - Added licence header - Removed unnecessary externs due merges of my contribution to Google Closure Compiler - Submodules update

--- app/app.js
+++ app/app.js
@@ -1,3 +1,26 @@
+//
+// Pubwebkit editor is powerful editor to create your ebook in various styles.
+// It's includes: Cover Designer, Template Editor, Community Snippets and more...
+// Also, it's a part of www.pubwebkit.com portal.
+// Copyright (C) 2014 Dmitry Antonenko
+//
+// This file is part of Pubwebkit editor
+//
+// Pubwebkit editor is free software: you can redistribute it and/or modify it under
+// the terms of the GNU Affero General Public License as published by the Free
+// Software Foundation, version 3
+//
+// Pubwebkit editor is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.
+//
+// See the GNU Affero General Public License for more details. You should have received
+// a copy of the GNU General Public License along with Hatch.js. If not, see
+// <http://www.gnu.org/licenses/>.
+//
+// Authors: Dmitry Antonenko
+//
+
 /**
  * @fileoverview Main file to initialize and prepare Pwk.Editor
  * @author Dmitry Antonenko <dmitry.antonenko@pubwebkit.com>
@@ -8,7 +31,47 @@ goog.provide('app');
 goog.require('app.Core');
 goog.require('app.exports');
 
-window.onload = function() {
-    // Run application
-    (new app.Core()).init();
+
+/**
+ * Cross-browser wrapper for DOMContentLoaded.
+ * @param {Window} win Window reference
+ * @param {Function} fn Callback function reference
+ * @private
+ */
+app.contentLoaded_ = function (win, fn) {
+    var done = false
+        , top = true
+        , doc = win.document
+        , root = doc.documentElement
+        , modern = doc.addEventListener
+        , add = modern ? 'addEventListener' : 'attachEvent'
+        , rem = modern ? 'removeEventListener' : 'detachEvent'
+        , pre = modern ? '' : 'on'
+        , init =
+            function(e) {
+                if (e.type == 'readystatechange' && doc.readyState != 'complete') return;
+                (e.type == 'load' ? win : doc)[rem](pre + e.type, init, false);
+                if (!done && (done = true)) fn.call(win, e.type || e);
+            }
+        , poll =
+            function() {
+                try { root.doScroll('left'); } catch(e) { setTimeout(poll, 50); return; }
+                init('poll');
+            };
+    if (doc.readyState == 'complete') {
+        fn.call(win, 'lazy');
+    } else {
+        if (!modern && root.doScroll) {
+            try { top = !win.frameElement; } catch(e) { }
+            if (top) poll();
+        }
+        doc[add](pre + 'DOMContentLoaded', init, false);
+        doc[add](pre + 'readystatechange', init, false);
+        win[add](pre + 'load', init, false);
+    }
 };
+
+// Run application
+app.contentLoaded_(window, function() {
+    (new app.Core()).init();
+});
--- app/pwk/editor/document.js
+++ app/pwk/editor/document.js
@@ -1,7 +1,29 @@
+//
+// Pubwebkit editor is powerful editor to create your ebook in various styles.
+// It's includes: Cover Designer, Template Editor, Community Snippets and more...
+// Also, it's a part of www.pubwebkit.com portal.
+// Copyright (C) 2014 Dmitry Antonenko
+//
+// This file is part of Pubwebkit editor
+//
+// Pubwebkit editor is free software: you can redistribute it and/or modify it under
+// the terms of the GNU Affero General Public License as published by the Free
+// Software Foundation, version 3
+//
+// Pubwebkit editor is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.
+//
+// See the GNU Affero General Public License for more details. You should have received
+// a copy of the GNU General Public License along with Hatch.js. If not, see
+// <http://www.gnu.org/licenses/>.
+//
+// Authors: Dmitry Antonenko
+//
+
 /**
  * @fileoverview Document contains all information about edited information and information itself,
  * as child components.
- *
  * @author Dmitry Antonenko <dmitry.antonenko@pubwebkit.com>
  */
 
